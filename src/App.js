e5c529fcce38c4535ea8a4548635c92cbf2f09ca
ValeriiaMur <lera-muradyan@yandex.com>
2020-11-10 16:45:44 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./images/logo-bonus.svg";
+import back from "./images/bg-pentagon.svg";
+import {useState} from "react";
 
 function App() {
+
+  const [score, setScore] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className="start">
+        <div class = "header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div class = "whiteSquare">
+            <h2>Score</h2>
+            <div id="score">{score}</div>
+          </div>
+        </div>
+        <div className="game">
+          <div className="board">
+            <div id="upper">
+              <div className = "circle" id = "sc"></div>
+            </div>
+            <div id = "middle">
+              <div className = "circle" id = "sc"></div>
+              <div className = "circle" id = "sc"></div>
+            </div>
+            <div id = "bottom">
+              <div className = "circle" id = "sc"></div>
+              <div className = "circle" id = "sc"></div>
+            </div>
+          </div>
+        </div>
+        
+        </body>
     </div>
   );
 }
